b3459883a556aaa73a8cd8291ad956628fae1c95
xtophe xanon <xocon10@gmail.com>
2020-06-19 13:29:23 -0500

    fix LS bug.  ready for deploy

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,70 +2,100 @@
 var date = moment().format('dddd, MMMM Do');
 $('#currentDay').text(date);
 
+// GLOBALS
 var containerEl = $('.container');
+// specify range of work day
 var dayStart = 9;
 var dayEnd = 17;
 
+// create an hour block for each hour in the workday
 for (var i = dayStart; i <= dayEnd; i++) {
-  createEvent(i);
+	createEvent(i);
 }
 
 // CREATE EVENT
 function createEvent(time) {
-  var hour = moment(time, 'h');
-  var hourDisplay = hour.format('hA');
-  var inputGroupEl = $('<div>').addClass('input-group')
-  var inputGroupPreEl = $('<div>').addClass('input-group-prepend');
-  var timeSpanEl = $('<span>').addClass('input-group-text hour').text(hourDisplay);
-  var eventEl = $('<textarea>').addClass('form-control').attr('data-id', time);
-  var inputGroupAppEl = $('<div>').addClass('input-group-append');
-  var saveBtnEl = $('<span>').addClass('input-group-text saveBtn').attr('data-id', time);
-  var calendarIconEl = $('<i>').addClass('fas fa-calendar-plus');
-  
-  inputGroupAppEl.append(saveBtnEl);
-  saveBtnEl.append(calendarIconEl);
-  inputGroupPreEl.append(timeSpanEl);
-  inputGroupEl.append(inputGroupPreEl, eventEl, inputGroupAppEl);
-  containerEl.append(inputGroupEl);
+	// create moment object for hour block at current iteration
+	var hour = moment(time, 'h');
+	// format as x:00AM/PM
+	var hourDisplay = hour.format('hA');
 
-  styleEventColor(hour, eventEl);
+	// build event block
+	var inputGroupEl = $('<div>').addClass('input-group');
+	var inputGroupPreEl = $('<div>').addClass('input-group-prepend');
+	var timeSpanEl = $('<span>').addClass('input-group-text hour').text(hourDisplay);
+	var eventEl = $('<textarea>').addClass('form-control').attr('data-id', time);
+	var inputGroupAppEl = $('<div>').addClass('input-group-append');
+	var saveBtnEl = $('<span>')
+		.addClass('input-group-text saveBtn')
+		.attr('data-id', time);
+	var calendarIconEl = $('<i>').addClass('fas fa-calendar-plus');
+
+	inputGroupAppEl.append(saveBtnEl);
+	saveBtnEl.append(calendarIconEl);
+	inputGroupPreEl.append(timeSpanEl);
+	inputGroupEl.append(inputGroupPreEl, eventEl, inputGroupAppEl);
+	containerEl.append(inputGroupEl);
+
+	// pass current hour block and event block
+	styleEventColor(hour, eventEl);
 }
 
-function styleEventColor(eventHour, eventEl){
-  var now = moment();
+// dynamically style event backgrounds based on relationship to current time
+function styleEventColor(eventHour, eventEl) {
+	var now = moment();
 
-  if (eventHour.isBefore(now, 'hour')) {
-    eventEl.addClass('past');
-  } else if (eventHour.isSame(now, 'hour')) {
-    eventEl.addClass('present');
-  } else {
-    eventEl.addClass('future');
-  }
+	if (eventHour.isBefore(now, 'hour')) {
+		eventEl.addClass('past');
+	} else if (eventHour.isSame(now, 'hour')) {
+		eventEl.addClass('present');
+	} else {
+		eventEl.addClass('future');
+	}
 }
 
-// SAVE EVENTS TO LOCAL STORAGE
+// SAVE EVENTS TO ARRAY AND LOCAL STORAGE
 $('.saveBtn').on('click', saveEvent);
 
-var events = [];
+var eventsArr = loadEvents() || [];
 
+// save event to array and localStorage
 function saveEvent() {
-  var eventText = $(this).parent().prev().val();
-  var eventObj = {
-    id: $(this).attr('data-id'),
-    eventText: eventText
-  }
-  events.push(eventObj);
-  localStorage.setItem('events', JSON.stringify(events));
+	// get text from textarea related to clicked saveBtn
+	var eventText = $(this).parent().prev().val().trim();
+
+	// build object with id value coming from saveBtn's data-id attribute
+	var eventObj = {
+		id        : $(this).attr('data-id'),
+		eventText : eventText
+	};
+
+	// new objects are pushed even if an existing object with that id is already in the array.  so there are duplicate entries if an event is updated.  remove object before pushing it.
+	// if the events array alredy has an object with the same id, splice it
+	for (var i = 0; i < eventsArr.length; i++) {
+		if (eventsArr[i].id === eventObj.id) {
+			eventsArr.splice(i, 1);
+		}
+	}
+
+	eventsArr.push(eventObj);
+
+	localStorage.setItem('eventsLS', JSON.stringify(eventsArr));
 }
 
+// RETRIEVE EVENTS FROM LOCALSTORAGE AND POPULATE UI
 function loadEvents() {
-  events = localStorage.getItem('events');
-  events = JSON.parse(events);
-  for (var i = 0; i < events.length; i++) {
-    $('.form-control[data-id="' + events[i].id + '"]').val(events[i].eventText);
-  }
+	var eventsLS = localStorage.getItem('eventsLS');
+	eventsLS = JSON.parse(eventsLS);
+	console.log(eventsLS);
+
+	if (!eventsLS) {
+		return false;
+	}
+
+	// loop over all events in LS array and poplulate event blocks that have data
+	for (var i = 0; i < eventsLS.length; i++) {
+		$('.form-control[data-id="' + eventsLS[i].id + '"]').val(eventsLS[i].eventText);
+	}
+	return eventsLS;
 }
-
-loadEvents();
-
-
